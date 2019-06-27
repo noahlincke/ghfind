daea2616c729a05b6b0b50d851a7d080a3d9c04c
Noah Lincke <noah@lincke.org>
2019-06-26 20:12:00 -0700

    Updated about

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this app</h1>
+      <p className="lead">
+        Searches Github users and retrieves information about their account
+      </p>
+      <p>Created by Noah Lincke as part of a tutorial by Brad Traversy</p>
+      <br />
+    </Fragment>
+  );
+};
+
+export default About;
